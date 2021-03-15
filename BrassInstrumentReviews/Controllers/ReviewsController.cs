14781dbe0cea4edc5aafc001a79b2a2ed68ca6a3
leseastingray <lesea@my.lanecc.edu>
2021-03-14 20:07:08 -0700

    Api GETs working

--- BrassInstrumentReviews/Controllers/ReviewsController.cs
+++ BrassInstrumentReviews/Controllers/ReviewsController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BrassInstrumentReviews.Models;
 
 namespace BrassInstrumentReviews.Controllers
 {
@@ -11,5 +15,42 @@ namespace BrassInstrumentReviews.Controllers
     [ApiController]
     public class ReviewsController : ControllerBase
     {
+        private InstrumentReviewContext context;
+
+        public ReviewsController(InstrumentReviewContext context)
+        {
+            this.context = context;
+        }
+
+        /* action methods */
+        // GET Reviews
+        [HttpGet]
+        public IActionResult GetReviews()
+        {
+            var reviews = context.Reviews.ToList();
+
+            if (reviews.Count == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(reviews);
+            }
+        }
+        // GET a Review - Api/Review
+        [HttpGet("{id}")]
+        public IActionResult GetReview(int id)
+        {
+            var review = context.Reviews.Find(id);
+            if (review ==  null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(review);
+            }
+        }
     }
 }
